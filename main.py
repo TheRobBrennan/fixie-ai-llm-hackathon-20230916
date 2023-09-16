5114bcda874759dca3c874b75c84b305f3581845
Rob Brennan <rob@therobbrennan.com>
2023-09-16 15:43:24 -0700

    Incorporated modularized environment utilities into main.py

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-# main.py
-from modules.dirA.moduleA import function_from_moduleA
-from modules.dirB.moduleB import function_from_moduleB
+from modules.environment.environment_utilities import (
+    load_environment_variables,
+    verify_environment_variables,
+)
 
-print(function_from_moduleA())
-print(function_from_moduleB())
+try:
+    # Load environment variables using the utility
+    env_vars = load_environment_variables()
+
+    # Verify the environment variables
+    if not verify_environment_variables(env_vars):
+        raise ValueError("Some environment variables are missing!")
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
