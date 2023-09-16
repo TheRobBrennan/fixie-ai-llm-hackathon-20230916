50f20d7b05f4d2b7b51e0fe560e602daa61d8490
Rob Brennan <rob@therobbrennan.com>
2023-09-16 15:50:13 -0700

    Add scaffolding for executing our three steps

--- main.py
+++ main.py
@@ -3,6 +3,31 @@ from modules.environment.environment_utilities import (
     verify_environment_variables,
 )
 
+
+def load_data_from_wikipedia_and_store_openai_embeddings_in_neo4j_vector():
+    try:
+        print(
+            f"Loading data from Wikipedia and store OpenAI embeddings in a Neo4j Vector"
+        )
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+
+def query_against_an_existing_neo4j_vector():
+    try:
+        print(f"Query against an existing Neo4j Vector")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+
+def question_answer_workflow_with_langchain():
+    try:
+        print(f"Question-Answer Workflow With LangChain")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+
+# Main program
 try:
     # Load environment variables using the utility
     env_vars = load_environment_variables()
@@ -10,5 +35,11 @@ try:
     # Verify the environment variables
     if not verify_environment_variables(env_vars):
         raise ValueError("Some environment variables are missing!")
+
+    # Here we go!
+    load_data_from_wikipedia_and_store_openai_embeddings_in_neo4j_vector()
+    query_against_an_existing_neo4j_vector()
+    question_answer_workflow_with_langchain()
+
 except Exception as e:
     print(f"An unexpected error occurred: {e}")
